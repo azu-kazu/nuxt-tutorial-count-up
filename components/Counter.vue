b80e9c4de7e4379dbb4ded71be80c70d97a3db58
azukazu <azu.kazu.max@gmail.com>
2021-05-24 02:39:04 +0900

    first commit

---
+++ components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div>
+      counter: {{ $store.state.counter.counter }}
+      <button @click="$store.commit('counter/countUp')">+1</button>
+      <button @click="$store.commit('counter/reset')">reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: "Counter App",
+    };
+  },
+};
+</script>
